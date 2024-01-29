b02dd82f94a50c34aa7d49a4af9f23fbb37f59a2
AssKissStudio <156083318+AssKissStudio@users.noreply.github.com>
2024-01-29 17:28:21 +0300

    New version in "releases"

---
+++ scmfndr_joint.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'scmfndr_joint.exe' with Python DLL being found.
+set PATH=C:\PROGRA~1\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Program Files\Python310
+"%~dp0scmfndr_joint.exe" %*
